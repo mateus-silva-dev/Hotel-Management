a48a9735c8fe7c6c97c1ecffa5d265835be432c1
Mateus Silva <252369372+mateus-silva-dev@users.noreply.github.com>
2026-08-19 18:39:53 -0300

    Database seeding

---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO tb_person (uuid, first_name, surname, document, birth_date, email, phone_number, mobile_number) VALUES ('1ace1347-2f4a-4db9-88e7-36d2bac76a8c', 'Maria',  'Ferreira Pereira', '42957390060', '1962-07-19', 'maria@email.com', '6821879908', '11913075437');
+INSERT INTO tb_person (uuid, first_name, surname, document, birth_date, email, phone_number, mobile_number) VALUES ('a92f715a-2f1a-4019-95d2-138a09aa0421', 'Otávio',  'César Ian Martins', '18257746843', '1992-07-23', 'otavio@email.com', '1142010307', '11967423789');
+
+INSERT INTO tb_employee (registration_code, hire_date, status) VALUES ('01-2026-0001', '2026-05-20', 'ACTIVE');
+INSERT INTO tb_employee (registration_code, hire_date, status) VALUES ('01-2026-0002', '2026-05-21', 'ACTIVE');
+
+INSERT INTO tb_job_position (name, active) VALUES ('RECEPCIONISTA', 'true');
+INSERT INTO tb_job_position (name, active) VALUES ('PORTEIRO', 'true');
