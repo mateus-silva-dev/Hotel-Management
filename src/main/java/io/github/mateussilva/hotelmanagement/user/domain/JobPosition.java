9750caa9b52b7be072574ad8d107b9d816dfd34d
Mateus Silva <252369372+mateus-silva-dev@users.noreply.github.com>
2026-08-19 18:41:35 -0300

    Job position

---
+++ src/main/java/io/github/mateussilva/hotelmanagement/user/domain/JobPosition.java
@@ -0,0 +1,59 @@
+package io.github.mateussilva.hotelmanagement.user.domain;
+
+import io.github.mateussilva.hotelmanagement.user.domain.exception.InvalidJobPositionException;
+import io.github.mateussilva.hotelmanagement.user.domain.exception.InvalidPersonException;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_job_position")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class JobPosition {
+
+    private static final String NAME_REQUIRED_MESSAGE = "Nome inválido";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private UUID uuid;
+
+    @Column(unique = true)
+    private String name;
+
+    private boolean active = true;
+
+
+    private JobPosition(String name) {
+        this.uuid = UUID.randomUUID();
+
+        if (name == null || name.isBlank() || name.length() < 2)
+            throw new InvalidJobPositionException(NAME_REQUIRED_MESSAGE);
+
+        this.name = name.toUpperCase();
+    }
+
+    public static JobPosition of(String name) {
+        return new JobPosition(name);
+    }
+
+
+    @Override
+    public final boolean equals(Object o) {
+        if (!(o instanceof JobPosition that)) return false;
+
+        return Objects.equals(getUuid(), that.getUuid());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getUuid());
+    }
+}
---
+++ src/main/java/io/github/mateussilva/hotelmanagement/user/domain/exception/InvalidJobPositionException.java
@@ -0,0 +1,9 @@
+package io.github.mateussilva.hotelmanagement.user.domain.exception;
+
+public class InvalidJobPositionException extends RuntimeException {
+
+    public InvalidJobPositionException(String message) {
+        super(message);
+    }
+
+}
