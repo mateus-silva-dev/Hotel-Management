b049fda0364355752b74eefb10d633b0c4dcf44e
Mateus Silva <252369372+mateus-silva-dev@users.noreply.github.com>
2026-08-19 18:38:18 -0300

    Employee

---
+++ src/main/java/io/github/mateussilva/hotelmanagement/user/domain/exception/InvalidEmployeeException.java
@@ -0,0 +1,9 @@
+package io.github.mateussilva.hotelmanagement.user.domain.exception;
+
+public class InvalidEmployeeException extends RuntimeException {
+
+    public InvalidEmployeeException(String message) {
+        super(message);
+    }
+
+}
