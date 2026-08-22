3d1da65f3dc0382aa23048dc5b07588bf28f07a9
Mateus Silva <252369372+mateus-silva-dev@users.noreply.github.com>
2026-08-22 19:13:38 -0300

    Email business rule tests

---
+++ src/test/java/io/github/mateussilva/hotelmanagement/shared/EmailTest.java
@@ -0,0 +1,9 @@
+package io.github.mateussilva.hotelmanagement.shared;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class EmailTest {
+
+}
